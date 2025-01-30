e2f357d6c643b0e020bd1330d333e6c46ac69e9a
Christophe <cguegan@gmail.com>
2025-01-30 17:24:04 +0100

    Initial Commit

---
+++ IosAppScaffolding/IosAppScaffoldingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppScaffoldingApp.swift
+//  IosAppScaffolding
+//
+//  Created by Christophe Guégan on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct IosAppScaffoldingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
